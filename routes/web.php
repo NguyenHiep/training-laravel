89d071aeb6fc12d859cf741d5dc8be3e2ea40e5a
NguyenHiep <minhhiep.q@gmail.com>
2019-06-20 04:23:26 +0700

    [change] Tim hieu routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,33 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+//
+Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/manage', function ()    {
+        // User đăng nhập mới vào được manage
+    });
+
+    Route::post('comments/create', function () {
+        // User đăng nhập mới được bình luận
+    });
+});
+
+Route::namespace('Manage')->group(function () {
+    // Controllers Within The "App\Http\Controllers\Manage" Namespace
+});
+
+Route::prefix('manage')->group(function () {
+    Route::get('users', function () {
+        // Matches The "/manage/users" URL
+    });
+});
+
+// Sử dụng kết hợp
+
+Route::middleware(['auth'])->prefix('manage')->group(function () {
+    Route::get('posts', function () {
+        // User đăng nhập mới vào được manage/posts
+    });
+});
