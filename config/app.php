554d1d33c9e95a500b7b9cff7d2ac669863e4192
NguyenHiep <minhhiep.q@gmail.com>
2019-06-25 08:04:35 +0700

    [change] laravel log viewer

--- config/app.php
+++ config/app.php
@@ -174,6 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // Package add new
+        Arcanedev\LogViewer\LogViewerServiceProvider::class,
 
     ],
 
